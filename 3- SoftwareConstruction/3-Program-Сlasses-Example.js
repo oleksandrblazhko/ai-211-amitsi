78931fe04105e7067a12d9f98bc65923b54d181b
D0907A <101993484+D0907A@users.noreply.github.com>
2023-11-10 01:08:31 +0200

    Create 3-Program-Сlasses-Example.js

---
+++ 3- SoftwareConstruction/3-Program-Сlasses-Example.js
@@ -0,0 +1,48 @@
+class User {
+    constructor(userId, name, surname, age) {
+        this.userId = userId;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+}
+
+class House {
+    constructor(houseId, address, price) {
+        this.houseId = houseId;
+        this.address = address;
+        this.price = price;
+    }
+}
+
+class Agreement {
+    constructor(agreementId, houseId, userId) {
+        this.agreementId = agreementId;
+        this.houseId = houseId;
+        this.userId = userId;
+    }
+}
+
+class SportEvent {
+    constructor(sporteventId, broadcastTime) {
+        this.sporteventId = sporteventId;
+        this.broadcastTime = broadcastTime;
+    }
+}
+
+class Notification {
+    constructor(notificationID, sporteventID, message, timeToSportevent) {
+        this.notificationID = notificationID;
+        this.sporteventID = sporteventID;
+        this.message = message;
+        this.timeToSportevent = timeToSportevent;
+    }
+}
+
+class UserNotification {
+    constructor(id, userID, notificationID) {
+        this.id = id;
+        this.userID = userID;
+        this.notificationID = notificationID;
+    }
+}
