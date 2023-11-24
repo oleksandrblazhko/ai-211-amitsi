aa539995d84af6e9072fa5d55a0a32b5422c3f0a
D0907A <101993484+D0907A@users.noreply.github.com>
2023-11-24 10:09:23 +0200

    Update TestCases.js

--- 3- SoftwareConstruction/3.3-ContinuousIntegration/TestCases.js
+++ 3- SoftwareConstruction/3.3-ContinuousIntegration/TestCases.js
@@ -12,13 +12,13 @@ function sendToUser(userId, messageText) {
 }
 
 const result1 = sendToUser(95, 'Send new message');
-console.log(`sendToUser(95, 'Send new message') - очікуваний результат 1 ${result1 === 1 ? 'TC1.1 passed' : 'TC1.1 failed'}`);
+console.log(`sendToUser(95, 'Send new message') - очікуваний результат 1 ${result1 === 1 ? 'TC1.1 passed' : (() => { throw new Error('TC1.1 failed') })()}`);
 
 const result2 = sendToUser(0, 'Send new message');
-console.log(`sendToUser(0, 'Send new message') - очікуваний результат -1 ${result2 === -1 ? 'TC1.2 passed' : 'TC1.2 failed'}`);
+console.log(`sendToUser(0, 'Send new message') - очікуваний результат -1 ${result2 === -1 ? 'TC1.2 passed' : (() => { throw new Error('TC1.2 failed') })()}`);
 
 const result3 = sendToUser(95, '');
-console.log(`sendToUser(95, '') - очікуваний результат 0 ${result3 === 0 ? 'TC1.3 passed' : 'TC1.3 failed'}`);
+console.log(`sendToUser(95, '') - очікуваний результат 0 ${result3 === 0 ? 'TC1.3 passed' : (() => { throw new Error('TC1.3 failed') })()}`);
 
 const result4 = sendToUser(0, '');
-console.log(`sendToUser(0, '') - очікуваний результат -2 ${result4 === -2 ? 'TC1.4 passed' : 'TC1.4 failed'}`);
+console.log(`sendToUser(0, '') - очікуваний результат -2 ${result4 === -2 ? 'TC1.4 passed' : (() => { throw new Error('TC1.4 failed') })()}`);
